d8463dcabbb23a4e7668318d330ac8ef0a855e9d
ayush <ayushtiwariiitr@gmail.com>
2014-05-20 18:42:22 +0530

    PEP8 included in extract script and more supports added

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from extract import extract
 import unittest
+import csv,re,os
 
 class TestProductinfoExtractor(unittest.TestCase):
    
@@ -8,7 +9,7 @@ class TestProductinfoExtractor(unittest.TestCase):
    
    def testCheckUrl(self):
      '''
-       Validates the url
+       Validates the url check incorporated in the extract function
      '''
      urls_for_validation = ['google.com','https://flipkart.com/ayush','https://amazon.com']
      with self.assertRaises(NameError) as context:
@@ -18,7 +19,8 @@ class TestProductinfoExtractor(unittest.TestCase):
     
    def testCheckFlipkart(self):
      '''
-       The extract function should return a dict on execution with urls having the xpath in the website_csv list
+       The extract function should return a dict on execution with 
+       urls having the xpath in the website_csv list
      '''
      item_name = "Scullers Men's Checkered Casual Shirt"
      item_name_extracted = str(extract('http://www.flipkart.com/scullers-men-s-checkered-casual-shirt/p/itmduvc4fpgtktkf?pid=SHTDUJF6XSSNB92T&srno=b_1&ref=884be278-844c-4a29-b300-b0c131dfddb0')['name']).replace("\n","").replace(" ","")
@@ -31,6 +33,17 @@ class TestProductinfoExtractor(unittest.TestCase):
     
      self.assertEqual(item_name.replace(" ","") , item_name_extracted)
 
-
+   def testCheckCsvContent(self):
+     '''
+       Check if the contents in the csv files are in proper way 
+     '''
+     input_file = open(os.path.join(os.path.dirname(__file__),'webpage_xpath.csv'), 'rb')
+     reader = csv.reader(input_file)
+     row = list(reader)
+     for r in range(len(row)):
+       for p in range(1,4):
+         if re.match('//',row[r][p]):
+            pass
+ 
 if __name__ == '__main__':
   unittest.main()
